205d5e5178179f67f11ef1e444a8c9ba08659b5b
Jan <janofthings@gmail.com>
2023-04-28 17:51:37 +0200

    data table view in options

--- src/options.js
+++ src/options.js
@@ -2,6 +2,20 @@
 window.onload = function () {
     chrome.storage.local.get(["filterList"], (result) => {
         document.getElementById("output").value = JSON.stringify(result, null, " ");
+        result.filterList.forEach((row, key) => {
+            let thisRow = document.createElement("tr");
+            let thisColumn = document.createElement("td");
+            thisColumn.innerText=key;
+            thisRow.appendChild(thisColumn);
+            ["search", "replace"].forEach((field, fieldKey) => {
+                let thisColumn = document.createElement("td");
+                thisColumn.innerText=row[field];
+                thisColumn.classList.add("row" + key);
+                thisColumn.classList.add("column" + fieldKey);
+                thisRow.appendChild(thisColumn);
+            });
+            document.getElementById("filterList").appendChild(thisRow);
+        });
     });
     document.getElementById("save").onclick = function () {
 
